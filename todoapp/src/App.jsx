da75c74f5f8dc6ae42c8d19f1924e12f164185c3
gaurav <gauravsoni8414@gmail.com>
2024-05-02 09:33:11 +0530

    ui set for todo app

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -26,14 +26,14 @@ const App = () => {
             <div className="flex items-center ">
               <button
                 type="button"
-                className=":-none text-white bg-green-700 hover:bg-green-800 :ring-4 :ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark::ring-green-800 me-2"
+                className=":-none text-white bg-green-700 hover:bg-green-800 :ring-4 :ring-green-300 font-medium rounded text-sm py-1  px-3    dark:bg-green-600 dark:hover:bg-green-700 dark::ring-green-800 me-2"
               >
                 Edit
               </button>
 
               <button
                 type="button"
-                className=":-none text-white bg-red-700 hover:bg-red-800 :ring-4 :ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-red-600 dark:hover:bg-red-700 dark::ring-red-900"
+                className=":-none text-white bg-red-700 hover:bg-red-800 :ring-4 :ring-red-300 font-medium rounded text-sm px-5 py-1  px-3  dark:bg-red-600 dark:hover:bg-red-700 me-3 dark::ring-red-900"
               >
                 Del
               </button>
