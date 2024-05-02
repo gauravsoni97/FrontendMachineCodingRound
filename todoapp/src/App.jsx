7a5d1518391a0676c75b0a620e0b0441ea54a206
gaurav <gauravsoni8414@gmail.com>
2024-05-02 09:29:12 +0530

    todo app ui set

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <form className="max-w-md mx-auto mt-4">
+        <div className="relative">
+          <input
+            type="search"
+            className="block w-full p-4  text-sm text-gray-900  rounded-lg bg-gray-50  dark:bg-gray-700  dark:text-white"
+            placeholder="todo..."
+            required
+          />
+          <button
+            type="submit"
+            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 :ring-4 :-none :ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark::ring-blue-800"
+          >
+            + Add
+          </button>
+        </div>
+      </form>
+      <center className="mt-10">
+        <ul className="max-w-md ">
+          <li className="w-full px-4 py-2 rounded  flex  items-center justify-between mb-6  dark:bg-gray-800 dark:border-gray-700 dark:text-white">
+            <div>Profile</div>
+            <div className="flex items-center ">
+              <button
+                type="button"
+                className=":-none text-white bg-green-700 hover:bg-green-800 :ring-4 :ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark::ring-green-800 me-2"
+              >
+                Edit
+              </button>
 
-export default App
+              <button
+                type="button"
+                className=":-none text-white bg-red-700 hover:bg-red-800 :ring-4 :ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2  dark:bg-red-600 dark:hover:bg-red-700 dark::ring-red-900"
+              >
+                Del
+              </button>
+            </div>
+          </li>
+        </ul>
+      </center>
+    </div>
+  );
+};
+
+export default App;
